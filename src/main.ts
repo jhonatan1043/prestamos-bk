2fd6a4996214551b87d137d1064eae9eb3ec31ca
Poseidon <42254061+jhonatan1043@users.noreply.github.com>
2025-09-25 08:41:33 -0500

    Update main.ts
    corrgiendo el main

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,8 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 App corriendo en: http://localhost:3000/api`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 App corriendo en: http://localhost:${port}/api`);
 }
 bootstrap();
