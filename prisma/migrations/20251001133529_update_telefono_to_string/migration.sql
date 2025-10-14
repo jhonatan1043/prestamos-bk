caa74ada52ca50ed5f6b11f3a9db9c10d676c2cc
samuel <samm-rojas@hotmail.com>
2025-10-14 12:36:03 -0500

    feat agregar relación de cliente y user con la tabla estado

--- prisma/migrations/20251001133529_update_telefono_to_string/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Cliente" ALTER COLUMN "telefono" SET DATA TYPE TEXT;
--- prisma/migrations/20251004234403_add_estado_softdelete/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Cliente" ADD COLUMN     "estado" TEXT NOT NULL DEFAULT 'ACTIVO';
-
--- AlterTable
-ALTER TABLE "Prestamo" ALTER COLUMN "estado" SET DEFAULT 'ACTIVO';
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "estado" TEXT NOT NULL DEFAULT 'ACTIVO';
