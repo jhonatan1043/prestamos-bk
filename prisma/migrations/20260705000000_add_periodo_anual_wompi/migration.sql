274ede27eec4cc4b0575695b494652d6d09deec8
jhonatan1043 <jhonat.rodri@gmail.com>
2026-07-10 23:29:51 -0500

    subiendo cambios

--- prisma/migrations/20260705000000_add_periodo_anual_wompi/migration.sql
+++ prisma/migrations/20260705000000_add_periodo_anual_wompi/migration.sql
@@ -1,2 +1,2 @@
--- AlterTable: agrega periodoAnual a WompiTransaccion
-ALTER TABLE "master"."WompiTransaccion" ADD COLUMN "periodoAnual" BOOLEAN NOT NULL DEFAULT false;
+-- AlterTable: agrega periodoAnual a WompiTransaccion (idempotente)
+ALTER TABLE "master"."WompiTransaccion" ADD COLUMN IF NOT EXISTS "periodoAnual" BOOLEAN NOT NULL DEFAULT false;
